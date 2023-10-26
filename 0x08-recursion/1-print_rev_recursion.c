02ec28c90d27b738f23a59b65d1404adacc42d78
HKDE <hognonkossidavid@gmail.com>
2023-10-26 15:42:28 +0000

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * @s: parametre
-*/
+  * @s: Function parameter
+  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0') {
-		return;  // Si le caractère actuel est '\0', retourne sans rien imprimer
+	if (*s)
+	{
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 	}
-	_print_rev_recursion(s + 1);  // Appel récursif avec le reste de la chaîne
-	putchar(*s);  // Imprime le caractère actuel après l'appel récursif
 }
