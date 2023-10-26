a5919074dbbdc04ff0c09d39949047158631485b
HKDE <hognonkossidavid@gmail.com>
2023-10-26 15:30:54 +0000

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,10 +9,11 @@
   */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') {
-		putchar('\n');  // Si le caractère actuel est '\0', imprime un saut de ligne
-		return;
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	putchar(*s);  // Imprime le caractère actuel
-	_puts_recursion(s + 1);  // Appel récursif avec le reste de la chaîne
+	else
+		_putchar('\n');
 }
