f81a5ef8cb291b1d9136b3512d151896628e17c1
HKDE <hognonkossidavid@gmail.com>
2023-10-26 15:25:28 +0000

    Message de commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+#include "main.h"
+/**
+  * _puts_recursion - main function
+  *
+  * @s: function parameter
+  *
+  * Return: Always 0.
+  */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0') {
