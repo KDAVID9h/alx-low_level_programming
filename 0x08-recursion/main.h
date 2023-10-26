d77510f25d2ddbd83390f20f4aaf7ba76fa361b5
HKDE <hognonkossidavid@gmail.com>
2023-10-26 18:51:28 +0000

    100,101

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int _sqrt_recursion(int n);
 
 #endif
