5ffd1a88525dc0687719eebe2e165795bcc81d40
KDAVID9h <hognonkossidavid@gmail.com>
2024-01-16 01:20:31 +0000

    1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -shared -o liball.so -fPIC *.c
+gcc -Wall -Werror -Wextra -pedantic  -c -fPIC *.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
