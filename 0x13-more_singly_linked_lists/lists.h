d8bb7e475f89430442e4751b6d8240a19abc0ec5
KDAVID9h <hognonkossidavid@gmail.com>
2024-03-04 16:27:22 +0000

    nice

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,8 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -9,7 +10,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for ALX project - More singly linked lists
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -17,39 +18,33 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Function prototypes */
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
-/* 0. Print list */
 size_t print_listint(const listint_t *h);
-
-/* 1. List length */
 size_t listint_len(const listint_t *h);
-
-/* 2. Add node */
 listint_t *add_nodeint(listint_t **head, const int n);
-
-/* 3. Add node at the end */
 listint_t *add_nodeint_end(listint_t **head, const int n);
-
-/* 4. Free list */
 void free_listint(listint_t *head);
-
-/* 5. Free list (set head to NULL) */
 void free_listint2(listint_t **head);
-
-/* 6. Pop head */
 int pop_listint(listint_t **head);
-
-/* 7. Get node at index */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-
-/* 8. Sum list */
 int sum_listint(listint_t *head);
-
-/* 9. Insert at index */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-
-/* 10. Delete at index */
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
-#endif /* LISTS_H */
+#endif
